2cc0b7a1540cdab5b90262488b639c7ad6550000
JakubTomaszewski <jakub.k.tomaszewski@gmail.com>
2020-02-12 17:59:22 +0100

    done the main reg, still tasks remaining

---
+++ LinearRegression/linreg_visualization.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+#preparing data
+df = pd.read_csv('student-mat.csv', sep=';')
+
+#instantiate model
+linreg = LinearRegression()
+
+x_train, x_test, Y_train, Y_test = train_test_split(df['G1'].values, df['G3'].values, test_size=0.2)
+x_train = x_train.reshape(-1,1)
+x_test = x_test.reshape(-1,1)
+
+linreg.fit(x_train, Y_train)
+
+print('The coefficient is: {}\nand the intercept: {}'.format(linreg.coef_, linreg.intercept_))
+
+#model visualization
+import matplotlib.pyplot as plt
+
+plt.scatter(x_test, Y_test, color='red', marker='+')
+plt.plot(x_test, linreg.predict(x_test), color='blue')
+
+plt.xlabel('G1', fontsize=15)
+plt.ylabel('G3', fontsize=15)
+plt.show()
